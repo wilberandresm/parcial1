d5888a266c03c473058b4d39643103c3e31a355d
ceisutb07 <ceisutb07@iMac-de-ceisutb07.local>
2019-09-05 11:11:39 -0500

    second commit

--- app.py
+++ app.py
@@ -3,13 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import send_file,request     
 from flask import make_response,send_file
 import os
-
+import time
+import pandas as pd
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    tiempo_actual = time.strftime("%c")
+    data = {'fecha':[tiempo_actual], 'tiempo': [2.22], 'temperatura': [22.7], 'humedad': [33.2]}
+    df = pd.DataFrame(data, columns= ['fecha', 'tiempo', 'temperatura', 'humedad'])
+    df.to_csv("09052019.csv")
+    return render_template('index.html')    
+    
 
 @app.route('/log',methods= ['GET'])
 def lista(): 
@@ -19,31 +26,9 @@ def lista():
         tiempo=request.args.GET('tiempo')
     except:
         return render_template('formulario.html')
-@app.route('/log')
-def return_file():
-    return send_file('/Users/ceisutb07/Desktop/PageSoftware-master/archivo.csv',attachment_filename='archivo.csv')
 
-@app.route('/log/',methods=['GET','POST'])
-def file_downloads():
-    if request.method == 'POST':
- # save the single "profile" file
-        profile = request.files['profile']
-        profile.save(os.path.join(uploads_dir, secure_filename(profile.filename)))
- # save each "charts" file
-        for file in request.files.getlist('charts'):
-            file.save(os.path.join(uploads_dir, secure_filename(file.name)))
-        return redirect(url_for('localhost:80/log'))
-    return render_template('fomrulario.html')
-        
-@app.route("/log")
-def plot_csv():
-    return send_file('outputs/Adjacency.csv',
-                     mimetype='text/csv',
-                     attachment_filename='Adjacency.csv',
-                     as_attachment=True)
+   
 
-    
-    
 
 if __name__ == "__main__":
     app.run(debug= True, host= '0.0.0.0', port=80)
